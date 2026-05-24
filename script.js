cab4aa39958e29a4d72f303862aedde359cf2e23
dziwnyczlowiekk-wq <dziwnyczlowiekk@gmail.com>
2026-05-24 09:11:40 +0200

    Update script.js

--- script.js
+++ script.js
@@ -497,7 +497,7 @@ function answer(chosen){
   document.getElementById('nxt').style.display = 'block';
 }
 var img = document.getElementById('qimg');
-}
+
   if(q.img){
   img.src = q.img;
   img.style.display = "block";
